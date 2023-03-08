b3b060c99670149ca83a7d0f3522acf70b62ebef
Artsiom_PH <workartpharm@gmail.com>
2023-03-08 22:34:06 +0300

    refactor models for test passing

--- main/models/tag.py
+++ main/models/tag.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Tag(models.Model):
-    id = models.PositiveIntegerField(primary_key=True, null=False, unique=True)
+    id = models.BigAutoField(primary_key=True, null=False, unique=True)
     title = models.CharField(max_length=255)
