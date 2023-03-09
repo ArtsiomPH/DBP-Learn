fd6cf2ad5cdff03ba135cc0291b6aca2af810cdc
Artsiom_PH <workartpharm@gmail.com>
2023-03-09 16:22:52 +0300

    refactor tag

--- main/models/tag.py
+++ main/models/tag.py
@@ -5,7 +5,7 @@ from .task import Task
 class Tag(models.Model):
     id = models.BigAutoField(primary_key=True, null=False, unique=True)
     title = models.CharField(max_length=255)
-    task = models.ManyToManyField(Task, null=True)
+    task = models.ManyToManyField(Task)
 
     def __str__(self):
         return self.title
