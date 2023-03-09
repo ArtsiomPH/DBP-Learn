8db7cfe1bf5528c7f50c5137a124cdb93b543990
Artsiom_PH <workartpharm@gmail.com>
2023-03-09 18:52:08 +0300

    reformat admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-<<<<<<< Updated upstream
-from django.contrib.auth.admin import UserAdmin
 from .models import User, Tag, Task
 
-<<<<<<< Updated upstream
-=======
-admin.site.register(User, UserAdmin)
-=======
-from .models import User, Tag, Task
-
->>>>>>> Stashed changes
 
 class TaskManagerAdminSite(admin.AdminSite):
     pass
@@ -20,20 +11,12 @@ task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
 
 @admin.register(Tag, site=task_manager_admin_site)
 class TagAdmin(admin.ModelAdmin):
-<<<<<<< Updated upstream
-    list_display = ('title', )
-=======
     list_display = ("title",)
->>>>>>> Stashed changes
 
 
 @admin.register(Task, site=task_manager_admin_site)
 class TaskAdmin(admin.ModelAdmin):
     list_display = (
-<<<<<<< Updated upstream
-        'title', 'description', 'creation_date', 'mod_date',
-        'deadline', 'status', 'priority', 'author', 'executor'
-=======
         "title",
         "description",
         "creation_date",
@@ -43,17 +26,9 @@ class TaskAdmin(admin.ModelAdmin):
         "priority",
         "author",
         "executor",
->>>>>>> Stashed changes
     )
 
 
 @admin.register(User, site=task_manager_admin_site)
 class UserAdmin(admin.ModelAdmin):
-<<<<<<< Updated upstream
-    list_display = (
-        'username', 'first_name', 'last_name', 'email', 'is_staff', 'role'
-    )
-=======
     list_display = ("username", "first_name", "last_name", "email", "is_staff", "role")
->>>>>>> Stashed changes
->>>>>>> Stashed changes
