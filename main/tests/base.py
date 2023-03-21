1769a9a8332dc3ea7465858606958bbb0154d0fc
Artsiom_PH <workartpharm@gmail.com>
2023-03-21 09:38:25 +0300

    add user tests

---
+++ main/tests/base.py
@@ -0,0 +1,70 @@
+from http import HTTPStatus
+from typing import Union, List
+
+from rest_framework.test import APIClient, APITestCase
+from rest_framework.response import Response
+from django.urls import reverse
+
+from main.models import User
+
+
+class TestViewSetBase(APITestCase):
+    user: User = None
+    client: APIClient = None
+    basename: str
+    user_attributes: dict
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        cls.admin = cls.create_superuser()
+        cls.user = cls.create_api_user()
+        cls.client = APIClient()
+
+    @staticmethod
+    def create_api_user():
+        return User.objects.create()
+
+    @staticmethod
+    def create_superuser():
+        return User.objects.create_superuser('admin')
+
+    @classmethod
+    def detail_url(cls, key: Union[int, str]) -> str:
+        return reverse(f"{cls.basename}-detail", args=[key])
+
+    @classmethod
+    def list_url(cls, args: List[Union[str, int]] = None) -> str:
+        return reverse(f"{cls.basename}-list", args=args)
+
+    def list(self, args: List[Union[str, int]] = None, kwargs: dict = None):
+        self.client.force_login(self.user)
+        response = self.client.get(self.list_url(args), kwargs)
+        assert response.status_code == HTTPStatus.OK
+        return response.json()
+
+    def create(self, data: dict, args: List[Union[str, int]] = None) -> dict:
+        self.client.force_login(self.user)
+        response = self.client.post(self.list_url(args), data=data)
+        assert response.status_code == HTTPStatus.CREATED, response.content
+        return response.data
+
+    def retrieve(self, key: Union[str, int] = None) -> dict:
+        self.client.force_login(self.user)
+        response = self.client.get(self.detail_url(key))
+        assert response.status_code == HTTPStatus.OK
+        return response.data
+
+    def update(self, data: dict, key: Union[str, int] = None) -> dict:
+        self.client.force_login(self.user)
+        response = self.client.patch(self.detail_url(key), data=data)
+        assert response.status_code == HTTPStatus.OK
+        return response.data
+
+    def delete(self, key: Union[str, int] = None) -> HTTPStatus:
+        self.client.force_login(self.admin)
+        response = self.client.delete(self.detail_url(key))
+        return response.status_code
+
+
+
---
+++ main/tests/test_user.py
@@ -0,0 +1,50 @@
+from main.tests.base import TestViewSetBase
+
+
+class TestUserViewSet(TestViewSetBase):
+    basename = "users"
+    user_attributes = {
+        'username': 'johnsmith',
+        'first_name': 'John',
+        'last_name': 'Smith',
+        'email': 'john@test.com',
+        'date_of_birth': '2000-01-01',
+        'phone': '+79000000000',
+        'role': 'developer'
+    }
+
+    @staticmethod
+    def expected_details(entity: dict, attributes: dict):
+        return {"id": entity["id"], **attributes}
+
+    def test_create(self):
+        user = self.create(self.user_attributes)
+        expected_response = self.expected_details(user, self.user_attributes)
+        assert user == expected_response
+
+    def test_retrieve(self):
+        user = self.create(self.user_attributes)
+        retrived_user = self.retrieve(user['id'])
+        assert user == retrived_user
+
+    def test_update(self):
+        for_update = {'email': 'john@test.ru'}
+        user = self.create(self.user_attributes)
+        updated_user = self.update(for_update, user['id'])
+        user.update(for_update)
+        assert user == updated_user
+        assert user['id'] == updated_user['id']
+
+    def test_delete(self):
+        user = self.create(self.user_attributes)
+        deleted_user = self.delete(user['id'])
+        assert deleted_user == 204
+
+    def test_filtration(self):
+        user = self.create(self.user_attributes)
+        expected_users = self.list(kwargs={'username': 'john'})
+        assert user == expected_users[0]
+        expected_users = self.list(kwargs={'username': 'alex'})
+        assert len(expected_users) == 0
+
+
