66d3c9eb2955cc637d7853de017792f1714ee94e
Artsiom_PH <workartpharm@gmail.com>
2023-04-03 13:57:20 +0300

    add new relic wrong

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from .settings import BASE_DIR
 
 from django.core.wsgi import get_wsgi_application
+import newrelic.agent
+
+newrelic.agent.initialize(os.path.join(BASE_DIR, 'newrelic.ini'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "task_manager.settings")
 
